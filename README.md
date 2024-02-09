57e0b0a8ba921820f1a771a5788e1dc5e29d83e5
GloriaBimbi <gloria.bimbi1@libero.it>
2024-02-09 19:03:39 +0100

    feat milestone 1, feat milestone 2

--- README.md
+++ README.md
@@ -5,3 +5,22 @@ This template should help get you started developing with Vue 3 in Vite. The tem
 ## Recommended IDE Setup
 
 - [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
+
+### CONSEGNA:
+
+## Milestone 1
+
+Create un componente Card predisposto per ricevere dall'esterno le informazioni necessarie alla visualizzazione dei propri elementi (con le props).
+Potete testarlo stampando una singola Card con informazioni statiche.
+
+## Milestone 2
+
+(Se non lo avete già fatto) definite un array di prodotti nel componente AppMain. Potete prendere spunto dall'array allegato o usarlo direttamente così com'è. Iterate sull'array di prodotti e per ognuno di essi create un componente Card, inviando le informazioni necessarie alla visualizzazione.
+
+## Bonus
+
+Facciamo in modo che il cuore nella card del prodotto sia colorato o vuoto a seconda del valore della proprietà "isInFavourites".
+
+## Bonus 2
+
+Spostate i menu dinamici dai componenti AppHeader e AppFooter al componente App. Inviateglieli poi tramite props.
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,4 +1,6 @@
 <script>
+import AppCard from "../components/AppCard.vue";
+
 export default {
   data() {
     return {
@@ -73,42 +75,14 @@ export default {
     };
   },
 
-  methods: {
-    buildImagePath(imageName) {
-      return new URL("../assets/img/" + imageName, import.meta.url).href;
-    },
-  },
+  components: { AppCard },
 };
 </script>
 
 <template>
   <main>
     <div id="product" class="container">
-      <div v-for="card in cards" class="card">
-        <img :src="buildImagePath(card.src)" alt="" class="product-picture" />
-        <div class="overlay">
-          <img
-            :src="buildImagePath(card.srcHover)"
-            alt=""
-            class="product-picture"
-          />
-        </div>
-        <span
-          >{{ card.brand }} <br />
-          <strong>{{ card.productDescription }}</strong> <br /><span
-            class="discount-prize"
-            >{{ card.discountPrize }}</span
-          >
-          <span class="original-prize">{{ card.originalPrize }}</span>
-        </span>
-        <span v-show="card.discountStatus" class="discount-percentage">{{
-          card.discountPercentage
-        }}</span>
-        <span v-show="card.sostenibility" class="sostenibility"
-          >Sostenibilità</span
-        >
-        <span class="heart">&hearts;</span>
-      </div>
+      <AppCard v-for="card in cards" :card="card"></AppCard>
     </div>
   </main>
 </template>
@@ -120,84 +94,5 @@ export default {
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
-
-  .card {
-    margin: 50px 25px;
-    width: calc(100% / 3 - 55px);
-    min-width: 100px;
-    cursor: pointer;
-    position: relative;
-
-    &:hover {
-      .overlay {
-        opacity: 1;
-      }
-    }
-
-    .product-picture {
-      max-width: 100%;
-      object-fit: contain;
-      display: block;
-      margin: 0 auto;
-    }
-
-    .discount-prize {
-      color: red;
-    }
-
-    .original-prize {
-      text-decoration: line-through;
-    }
-
-    .discount-percentage {
-      background-color: red;
-      color: white;
-      padding: 5px 10px;
-      position: absolute;
-      bottom: 100px;
-      left: 0;
-    }
-
-    .sostenibility {
-      color: white;
-      background-color: green;
-      padding: 5px 10px;
-      position: absolute;
-      bottom: 100px;
-      left: 60px;
-    }
-
-    .sostenibility-solo {
-      color: white;
-      background-color: green;
-      padding: 5px 10px;
-      position: absolute;
-      bottom: 100px;
-      left: 0;
-    }
-    .heart {
-      color: black;
-      background-color: white;
-      padding: 10px;
-      position: relative;
-      left: 154px;
-      bottom: 415px;
-
-      &:hover {
-        color: red;
-      }
-    }
-    .overlay {
-      position: absolute;
-      top: 0;
-      bottom: 0;
-      left: 0;
-      right: 0;
-      height: 80%;
-      width: 100%;
-      opacity: 0;
-      transition: 0.5s ease;
-    }
-  }
 }
 </style>
